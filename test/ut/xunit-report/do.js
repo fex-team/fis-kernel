8d532d2859be77e164475edf1e055014dd239901
ktmtwm <ktmtmw@163.com>
2013-09-03 11:11:40 +0800

    [case]删除xunit report.xml中install输出的信息

--- test/ut/xunit-report/do.js
+++ test/ut/xunit-report/do.js
@@ -1,21 +1,7 @@
 /*用来删除写到xunit report里的install信息*/
 
-var fs = require('fs');
-var file = "report.xml";
-var report = fs.readFileSync(file,'utf-8');
-var content = report.toString().split('\n');
-var i = 0;
-for(i=0; i<content.length;i++ ){
-    var reg = /\binstall.*/g ;
-    callback = function($1){
-        if($1){
-            content.splice(i,1);
-            i--;
-        }
-    };
-    content[i].replace(reg,callback);
-}
-for(i=0; i<content.length;i++ ){
-    content[i] = content[i]+'\n';
-}
-fs.writeFileSync(file, content,'utf-8');
+ var fs = require('fs');
+ var file = "report.xml";
+ var content = fs.readFileSync(file,'utf-8');
+ content = content.replace(/\binstall.*/gi,'');
+ fs.writeFileSync(file, content,"utf-8");
