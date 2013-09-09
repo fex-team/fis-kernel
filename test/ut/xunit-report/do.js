c8721fe6575a83294ca535debd5a4774efa242c6
ktmtwm <ktmtmw@163.com>
2013-09-09 13:03:54 +0800

    [case]修正xunit-report处理do.js

--- test/ut/xunit-report/do.js
+++ test/ut/xunit-report/do.js
@@ -3,5 +3,5 @@
  var fs = require('fs');
  var file = "report.xml";
  var content = fs.readFileSync(file,'utf-8');
- content = content.replace(/\binstall.*/gi,'');
+ content = content.replace(/\binstall.*\n/gi,'');
  fs.writeFileSync(file, content,"utf-8");
