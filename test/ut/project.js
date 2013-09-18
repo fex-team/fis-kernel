7cdf2ba85fd76f618e60fe280f2d5ca66d509b06
ktmtwm <ktmtwm@163.com>
2013-09-18 17:44:33 +0800

    [case]更新project.js用例

--- test/ut/project.js
+++ test/ut/project.js
@@ -7,8 +7,8 @@ var expect = require('chai').expect;
 
 describe('project',function(){
     it('getTempPath', function(){
-
-        var tmp = process.env['LOCALAPPDATA']+'/.fis-tmp';
-        expect(project.getTempPath()).to.be.equal(tmp);
+        var tmp = process.env['LOCALAPPDATA'];
+        tmp = tmp.replace(/\\/g, '/');
+        expect(project.getTempPath()).to.be.equal(tmp+'/.fis-tmp');
     });
 });
