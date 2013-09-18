9e31ae895b60efbb0233ee953629e29f438ebb8b
ktmtwm <ktmtwm@163.com>
2013-09-18 18:30:23 +0800

    [case]更新project.js测试用例

--- test/ut/project.js
+++ test/ut/project.js
@@ -1,13 +1,18 @@
-var fs = require('fs');
+var fs = require('fs'),
+    fis = require('../../fis-kernel.js');
 
-var fis = require('../../fis-kernel.js');
 var project = fis.project;
-
 var expect = require('chai').expect;
 
 describe('project',function(){
     it('getTempPath', function(){
-        var tmp = process.env['LOCALAPPDATA'];
+        var list = ['LOCALAPPDATA', 'APPDATA', 'HOME'];
+        var tmp;
+        for(var i = 0, len = list.length; i < len; i++){
+            if(tmp = process.env[list[i]]){
+                break;
+            }
+        }
         tmp = tmp.replace(/\\/g, '/');
         expect(project.getTempPath()).to.be.equal(tmp+'/.fis-tmp');
     });
